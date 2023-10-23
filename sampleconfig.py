707166fc793d96e873ca77de8a4a93f2e58d85a9
Cuh4 <roozzguy@gmail.com>
2023-10-23 20:50:01 +0100

    config example

---
+++ sampleconfig.py
@@ -0,0 +1,15 @@
+# //----------------------
+# configuration
+# //----------------------
+
+# bot
+token = "" # your bot's token
+
+# message fetching
+notifyInChannel = True # whether or not the bot sends a message in the desired channel upon fetching messages successfully
+excludeMessagesWithAttachments = True # whether or not to include messages with attachments
+
+dumpFilePath = "" # folder path to dump file, e.g: "C:/path/to/folder" (empty string = working directory)
+dumpFileName = "messages" # name of dump file. this will become "messages.json"
+
+# !! rename this to "config.py" !!
